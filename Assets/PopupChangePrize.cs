4bba4f06425288450afd11a5ef600857ac2b77a0
cristianscaliaarrow <30871073+cristianscaliaarrow@users.noreply.github.com>
2017-09-01 14:07:41 -0300

    PopUp
    Fixed MsgPopUping

--- Assets/PopupChangePrize.cs
+++ Assets/PopupChangePrize.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PopupChangePrize : MonoBehaviour {
+    public static PopupChangePrize instance;
+
     public static PrizeStaff prize;
     public InputField input;
     public Text textPts;
     public Text title;
     Action _a = delegate { };
 
-    public GameObject msgPanel;
+    public GameObject panelResult;
+
+
+    private void Awake()
+    {
+        instance = this;
+        print("popupChangePrize ON!");
+        gameObject.SetActive(false);        
+    }
 
     public void OnEnable()
     {
@@ -20,14 +30,14 @@ public class PopupChangePrize : MonoBehaviour {
 
     public void BTN_ChangePrize()
     {
-        if (int.Parse(input.text) <= 0) { ShowPanel("Error al ingresar el monto!", 1); return; }
+        if (int.Parse(input.text) <= 0) { PanelResult.ShowMsg("Error al ingresar el monto!", 1); return; }
         if (Login.debugUser.total_pts*2 >= int.Parse(input.text)) 
         {
             StartCoroutine(SendBuyPrize());
         }
         else
         {
-            ShowPanel("No Tiene Suficientes Puntos", 1,()=>BTN_ClosePopUp());
+            PanelResult.ShowMsg("No Tiene Suficientes Puntos", 1,()=>BTN_ClosePopUp());
         }
 
        
@@ -35,7 +45,7 @@ public class PopupChangePrize : MonoBehaviour {
 
     private IEnumerator SendBuyPrize()
     {
-        ShowPanel("Espere...",2,() => ShowPanel("La operacoin se realizo correctamente.", 2,()=> {
+        PanelResult.ShowMsg("Espere...",2,() => PanelResult.ShowMsg("La operacoin se realizo correctamente.", 2,()=> {
             int pesos = int.Parse(input.text);
             float decimals = (float.Parse(input.text) /2 - (int)int.Parse(input.text)/2);
             int discount = (int)(pesos / 2 + ((decimals > 0) ? 1 : 0));
@@ -54,17 +64,5 @@ public class PopupChangePrize : MonoBehaviour {
         input.text = "";
     }
 
-    public void ShowPanel(string msg,float time,Action a = null)
-    {
-        msgPanel.GetComponentInChildren<Text>().text = msg;
-        StartCoroutine(cShowPanelError(time,a));
-    }
-
-    private IEnumerator cShowPanelError(float time,Action a)
-    {
-        msgPanel.SetActive(true);
-        yield return new WaitForSeconds(time);
-        msgPanel.SetActive(false);
-        if (a != null) a();
-    }
+   
 }
--- Assets/Scripts/WriteQuery.cs
+++ Assets/Scripts/WriteQuery.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class WriteQuery : MonoBehaviour {
@@ -9,6 +13,8 @@ public class WriteQuery : MonoBehaviour {
     public InputField correo;
     public InputField tema;
     public InputField message;
+
+    public PopUpContactar popupContactar;
 
     public void ClearFields()
     {
@@ -20,8 +26,42 @@ public class WriteQuery : MonoBehaviour {
 
     public void BTN_Enviar()
     {
-        PhpQuery.SendContact(nombre,correo,tema,message);
-        ClearFields();
+        GameManager.instance.SendContactoUNITY(nombre.text,correo.text, tema.text, message.text);
     }
+
+    public IEnumerator SendContact(string nombre,string correo,string tema,string message)
+    {
+        var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://api.nextcode.ml/msjs");
+        httpWebRequest.ContentType = "application/json";
+        httpWebRequest.Method = "POST";
+        httpWebRequest.Headers.Add("Authentication", "Bearer " + User.authorization);
+
+        yield return null;
+        /*
+        user_id, subject, msj, from 
+        */
+        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+        {
+            string json = "{\"data\": {\"user_id\": \"" + Login.debugUser.id + "\",\"subject\": \"" + tema + "\",\"message\": \"" + message + "\",\"from\": \"" + correo + "\"}}";
+            streamWriter.Write(json);
+            yield return null;
+
+            streamWriter.Flush();
+            streamWriter.Close();
+        }
+        yield return null;
+
+        var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+        using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+        {
+            var result = streamReader.ReadToEnd();
+            yield return null;
+
+        }
+        yield return null;
+
+    }
+
+   
 }
 
